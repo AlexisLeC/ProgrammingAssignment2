48a55a110aacaab4dcca21c77e3664652a12666b
alexislc <alexis.le.couteulx@gmail.com>
2015-07-21 19:24:17 +0200

    Documentation added

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,35 @@
 ## These functions can be used to create a special matrix that can cached its inverse `makeCacheMatrix`
 ## and to compute and cached the inverse of this special matrix `cacheSolve`.
 
-## A function that creates an R object that can stored a matrix and its inverse
+## A function that creates a list that is used as a special "matrix" that can stored a matrix and cached its inverse
 ## It offers four methods:
 ## set(x) to set an new matrix x.
 ## get() to get the currently saved matrix or NULL if no matrix is saved.
 ## setInvMat(x) to cached the inverse matrix x.
-## getInvMat() to get the inverse matrix if computer and NULL otherwise.
+## getInvMat() to get the inverse matrix if it has already been computed and NULL otherwise.
+
+## Use example
+## >> aMat <- matrix(1:9, nrow=3, ncol=3)
+## >> aCacheMat <- makeCacheMatrix(aMat)
+## >> cacheSolve(aCacheMat) ## compute, cache and return the inverse
+## >> cacheSolve(aCacheMat) ## return the cache inverse
+
 makeCacheMatrix <- function(x = matrix()) {
 	myInvMat <- NULL
+
+	## Set an new matrix x.
 	set <- function(y) {
 		x <<- y
 		myInvMat <<- NULL
 	}
+
+	## Return the currently saved matrix or NULL if no matrix is saved
 	get <- function() x
+
+	## Cache the inverse matrix x.
 	setInvMat <- function(invMat) myInvMat <<- invMat
+
+	## Return the inverse matrix if it has already been computed and NULL otherwise
 	getInvMat <- function() myInvMat
 	list(set = set, get = get, setInvMat = setInvMat, getInvMat = getInvMat)
 }
@@ -29,6 +44,7 @@ cacheSolve <- function(x) {
 	if (!is.null(m)) {
 		return(m)
 	}
+	## So the matrix has not been computed yet
 	aMat <- x$get()
 	invMat <- solve(aMat)
 	x$setInvMat(invMat)
