7516f6178de46144a3ef6aca735306696afb81d7
alexislc <alexis.le.couteulx@gmail.com>
2015-07-21 19:09:52 +0200

    Implementation of a special matrix type that can be cached

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions can be used to create a special matrix that can cached its inverse `makeCacheMatrix`
+## and to compute and cached the inverse of this special matrix `cacheSolve`.
 
-## Write a short comment describing this function
-
+## A function that creates an R object that can stored a matrix and its inverse
+## It offers four methods:
+## set(x) to set an new matrix x.
+## get() to get the currently saved matrix or NULL if no matrix is saved.
+## setInvMat(x) to cached the inverse matrix x.
+## getInvMat() to get the inverse matrix if computer and NULL otherwise.
 makeCacheMatrix <- function(x = matrix()) {
-
+	myInvMat <- NULL
+	set <- function(y) {
+		x <<- y
+		myInvMat <<- NULL
+	}
+	get <- function() x
+	setInvMat <- function(invMat) myInvMat <<- invMat
+	getInvMat <- function() myInvMat
+	list(set = set, get = get, setInvMat = setInvMat, getInvMat = getInvMat)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## This function returns the inverse of a matrix (x).
+## If the inverse was not computed, it is computed and cached.
+## It assumes that x is invertible.
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$getInvMat()
+	if (!is.null(m)) {
+		return(m)
+	}
+	aMat <- x$get()
+	invMat <- solve(aMat)
+	x$setInvMat(invMat)
+	invMat
 }
